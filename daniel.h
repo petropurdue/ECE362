edf97f4e0a5d6e3c55f04775dcb185eeb8faf6ce
Daniel Hong <46686047+DanielH315@users.noreply.github.com>
2021-12-06 12:33:36 -0500

    Update daniel.h

--- daniel.h
+++ daniel.h
@@ -1,5 +1,13 @@
 #ifndef __DANIEL_H__
 #define __DANIEL_H__
 
+//mine
+void init_keyPad();
+void printCurrDir();
+void printFileList();
+void emptyFileList();
+void printCursor(int a);
+//void printString(char * string, int x, int p);
+FRESULT scan_files (char* path);
 
 #endif 
